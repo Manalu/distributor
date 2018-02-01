8dc5816a88f7c6184ed62792e5f5cf44a4385261
Tarek Ibne Showkot <16212165+imcody@users.noreply.github.com>
2018-02-02 00:52:40 +0600

    security update on cofig file

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']                                     = 'http://rafiz-agency.tk/';
+//$config['base_url']                                     = '';
 /*
 |--------------------------------------------------------------------------
 | Index File
